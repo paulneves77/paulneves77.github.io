a64fdd073e68b090a9b0e04229538e35706a2bf7
Paul Neves <paulneves77@gmail.com>
2021-01-29 13:22:26 -0500

    added more connections, changed dep_clr pics

--- 3D_graphs_VR/graphs/QLCI_OC_dep_clr/reconnect_nodes.py
+++ 3D_graphs_VR/graphs/QLCI_OC_dep_clr/reconnect_nodes.py
@@ -8,16 +8,19 @@ Created on Sat Jan 23 16:57:33 2021
 PI_list1 = ["Barak", "Choi", "Chuang", "Demler", "Hayden", "Lukin", "Natarajan", "Preskill", "Schleier-Smith", "Sudan", "Zwierlein"]
 PI_list2 = ["Englund", "Greiner", "Ketterle", "Zwierlein"]
 PI_list3 = ["Chandran", "Côté", "Greiner", "Lukin", "Ni", "Sachdev", "Schleier-Smith"]
-PI_list4 = ["Checkelsky", "Demler", "Jarillo-Herrero", "Kim", "Lukin", "Park", "Sachdev", "Todadri"]
-PI_list5 = ["Cappellaro", "Choi", "Englund", "Lukin", "Park", "Sushkov", "Todadri"]
+PI_list4 = ["Checkelsky", "Demler", "Jarillo-Herrero", "Kim", "Lukin", "Park", "Sachdev", "Todadri", "Vishwanath"]
+PI_list5 = ["Cappellaro", "Choi", "Englund", "Hu", "Lukin", "Park", "Sushkov", "Todadri"]
 PI_list6 = ["Barak", "Cappellaro", "Chandran", "Choi", "Englund", "Han", "Hu", "Lukin", "Notaros", "Preskill", "Vuckovic"]
+EC_list = ["Lukin", "Hu", "Ketterle", "Preskill", "Greiner", "Ni", "Cappellaro", "Kim", "Englund"]
+EDU_list = ["Cote", "Hu", "Checkelsky"]
 
-PI_lists= [PI_list1, PI_list2, PI_list3, PI_list4, PI_list5, PI_list6]
+PI_lists= [PI_list1, PI_list2, PI_list3, PI_list4, PI_list5, PI_list6, EC_list, EDU_list]
 
-for ind, PI_list in enumerate(PI_lists):
-    value = ind + 1
-    for i in range(len(PI_list)-1):
-        name_i = PI_list[i]
-        for j in range(len(PI_list)-i-1):
-            name_j = PI_list[i+j+1]
-            print(f'		{{"source": "{name_i}", "target": "{name_j}", "value": {value}}},')
+with open("edge_list.txt", "w") as f:
+	for ind, PI_list in enumerate(PI_lists):
+		value = ind + 1
+		for i in range(len(PI_list)-1):
+			name_i = PI_list[i]
+			for j in range(len(PI_list)-i-1):
+				name_j = PI_list[i+j+1]
+				f.write(f'		{{"source": "{name_i}", "target": "{name_j}", "value": {value}}},\n')
