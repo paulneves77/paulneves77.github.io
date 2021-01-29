28e36e1da940bbe6196a5b26d1de62127d7837b8
Paul Neves <paulneves77@gmail.com>
2021-01-29 15:30:40 -0500

    added more links

--- 3D_graphs_VR/graphs/QLCI_OC_dep_clr/reconnect_nodes.py
+++ 3D_graphs_VR/graphs/QLCI_OC_dep_clr/reconnect_nodes.py
@@ -13,11 +13,11 @@ PI_list5 = ["Cappellaro", "Choi", "Englund", "Hu", "Lukin", "Park", "Sushkov", "
 PI_list6 = ["Barak", "Cappellaro", "Chandran", "Choi", "Englund", "Han", "Hu", "Lukin", "Notaros", "Preskill", "Vuckovic"]
 EC_list = ["Lukin", "Hu", "Ketterle", "Preskill", "Greiner", "Ni", "Cappellaro", "Kim", "Englund"]
 EDU_list = ["Côté", "Hu", "Checkelsky"]
-PDF1_list = ["PDF1", "Greiner", "Demler"]
-PDF2_list = ["PDF2", "Ni", "Sachdev"]
-PDF3_list = ["PDF3", "Sudan", "Englund"]
-PDF4_list = ["PDF4", "Cappellaro", "Sushkov"]
-PDF5_list = ["PDF5", "Hayden", "Zwierlein"]
+PDF1_list = ["PDF1", "Greiner", "Demler", "Barak"]
+PDF2_list = ["PDF2", "Ni", "Sachdev", "Preskill"]
+PDF3_list = ["PDF3", "Sudan", "Englund", "Choi"]
+PDF4_list = ["PDF4", "Cappellaro", "Sushkov", "Chandran"]
+PDF5_list = ["PDF5", "Hayden", "Zwierlein", "Todadri"]
 
 PI_lists= [PI_list1, PI_list2, PI_list3, PI_list4, PI_list5, PI_list6, EC_list, EDU_list, PDF1_list, PDF2_list, PDF3_list, PDF4_list, PDF5_list]
 color_list = ["#7e1e9c", "#15b01a", "#0343df", "#ff81c0", "#653700", "#e50000", "#f97306", "#c20078", "#929591", "#929591", "#929591", "#929591", "#929591"]
@@ -30,4 +30,4 @@ with open("edge_list.txt", "w") as f:
 			name_i = PI_list[i]
 			for j in range(len(PI_list)-i-1):
 				name_j = PI_list[i+j+1]
-				f.write(f'		{{"source": "{name_i}", "target": "{name_j}", "color": "{color_list[ind]}", "width": {width_list[ind]}}},\n')
+				f.write(f',\n		{{"source": "{name_i}", "target": "{name_j}", "color": "{color_list[ind]}", "width": {width_list[ind]}}}')
